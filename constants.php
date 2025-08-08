73499224bd376be7931cb263d7b8e6a78c3bf52b
flook25 <flook11223344@gmail.com>
2025-08-08 21:48:27 +0700

    practice

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+define("score",500);
+define("PI",3.14);
+// $score=100;
+// $score=$score+200;
+
+$radius=6;
+$area=PI * $radius *$radius;
+
+echo "Area of cucle = ".$area." mm";
+
+
+?>
---
+++ variablefunc.php
@@ -0,0 +1,16 @@
+<?php
+$total1=null;
+$total2="";
+$total3=0;
+$total="Kittitouch Tantiwong";
+
+
+
+
+echo "variable total1 =".empty($total1)."<br>";
+echo "variable total2 =".empty($total2)."<br>";
+echo "variable total3 =".empty($total3)."<br>";
+echo "variable total4 =".empty($total)."<br>";
+
+
+?>
